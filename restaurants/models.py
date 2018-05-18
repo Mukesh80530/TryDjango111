97865276bb6eb2eec52bce1eaaf1d94d9c608539
Mukesh Gautam <mukeshgautam.er@gmail.com>
2018-05-19 01:33:39 +0530

    update app

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-class RestaurantLocation(models.Model):
-	name = models.CharField(max_length=120)
-	location = models.CharField(max_length=120, null=True, blank=True)
 
-	def __unicode__(self):
-		return name
+class RestaurantLocation(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120, null=True, blank=False)
+    category = models.CharField(max_length=120, null=True, blank=False)
+    timestamp =  models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
