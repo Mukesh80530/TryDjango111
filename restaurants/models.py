dd5ef0b434a2e6e46b04c1a3f4bae47ea4358b32
Mukesh Gautam <mukeshgautam.er@gmail.com>
2018-05-20 17:13:05 +0530

    this update in

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,7 +5,7 @@ class RestaurantLocation(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120, null=True, blank=False)
     category = models.CharField(max_length=120, null=True, blank=False)
-    timestamp =  models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- restaurants/views.py
+++ restaurants/views.py
@@ -7,10 +7,10 @@ from django.http import HttpResponse
 
 
 def home(request):
-	num =  random.randint(0,100000,)
-	some_list = [random.randint(1,100000), random.randint(1,100000), random.randint(1,10000)]
-	var = 'this is simples string'
-	return render(request, 'home.html', {'html_var':var,'num':num,'var': True, 'some_list':some_list})
+    num = random.randint(0, 100000, )
+    some_list = [random.randint(1, 100000), random.randint(1, 100000), random.randint(1, 10000)]
+    var = 'this is simples string'
+    return render(request, 'home.html', {'html_var': var, 'num': num, 'var': True, 'some_list': some_list})
 
 
 # def about(request):
@@ -27,16 +27,16 @@ class ContactClassView(View):
         context = {}
         return render(request, 'contact.html', context)
 
-    # def post(self, request,*args, **kwargs):
-    # 	context = {}
-    # 	return render(request, 'contact.html', context)
+        # def post(self, request,*args, **kwargs):
+        # 	context = {}
+        # 	return render(request, 'contact.html', context)
 
-    # def put(self, request,*args, **kwargs):
-    # 	context = {}
-    # 	return render(request, 'contact.html', context)
+        # def put(self, request,*args, **kwargs):
+        # 	context = {}
+        # 	return render(request, 'contact.html', context)
 
 
 def Retaurant_ListView(request):
-    queryset =  RestaurantLocation.objects.all()
-    context = {'object_list':queryset}
+    queryset = RestaurantLocation.objects.all()
+    context = {'object_list': queryset}
     return render(request, 'restaurants_list.html', context)
